c9581903c0db7dfd3975b147bc2b028742db6bf3
yuvrajgrover6 <yuvrajgrover6@gmail.com>
2024-02-07 14:49:03 +0530

    initialize repo
    Signed-off-by: yuvrajgrover6 <yuvrajgrover6@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
-if __name__ == '__main__':
-    print('PyCharm')
+import google.generativeai as genai
 
+genai.configure(api_key="AIzaSyBELsiXol5mvzDZTTBpFcuJWTOwLr05gfo")
 
-def hello():
-    print("ram")
+# Set up the model
+generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
+
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+]
+
+model = genai.GenerativeModel(
+    model_name="gemini-pro",
+    generation_config=generation_config,
+    safety_settings=safety_settings
+)
+
+while True:
+    # Get user prompt
+    user_prompt = input("Enter your prompt: ").strip()
+    prompt_parts = [user_prompt]
+    if user_prompt == "exit":
+        break
+    # Generate content based on user prompt
+    response = model.generate_content(prompt_parts)
+    print(response.text)
